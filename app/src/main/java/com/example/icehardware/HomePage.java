32be8fcbd750dfabbacb4a3e881f4d15f2b59042
Rifaldlee <103484674+Rifaldlee@users.noreply.github.com>
2022-12-16 22:33:56 +0700

    Update HomePage.java
    revisi

--- app/src/main/java/com/example/icehardware/HomePage.java
+++ app/src/main/java/com/example/icehardware/HomePage.java
@@ -5,13 +5,18 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class HomePage extends AppCompatActivity{
+    private ProgressBar spinner;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        spinner = (ProgressBar) findViewById(R.id.progressBar);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -21,5 +26,8 @@ public class HomePage extends AppCompatActivity{
                 finish();
             }
         },2500);
+    }
+    public void onPageFinished(View view){
+        spinner.setVisibility(View.GONE);
     }
 }
